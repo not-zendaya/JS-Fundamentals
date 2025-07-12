58f227d0867895a40c6c3b2b3a117b602c80c66f
not-zendaya <ukzendaya@gmail.com>
2025-07-12 13:31:21 +0300

    Fix input validation for argument check

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x) || x < 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
   for (let i = 0; i < x; i++) console.log("C is fun");
