02a928873eac315d141ca5d44885df35bfc28be8
not-zendaya <ukzendaya@gmail.com>
2025-07-12 12:44:17 +0300

    Script to convert agument to an integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,22 +1,8 @@
 const arg = process.argv[2];
-const num = parseInt(89);
+const num = parseInt();
 
 if (!isNaN(num)) {
   console.log(`My number: ${num}`);
-} else {
-  console.log("Not a number");
-}
-const num1 = parseInt(-4545);
-
-if (!isNaN(num1)) {
-  console.log(`My number: ${num1}`);
-} else {
-  console.log("Not a number");
-}
-const num2 = parseInt("ALX");
-
-if (!isNaN(num2)) {
-  console.log(`My number: ${num2}`);
 } else {
   console.log("Not a number");
 }
