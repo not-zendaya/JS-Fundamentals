471cb3d555175aefbc527ee654b29adde9fb94fb
not-zendaya <ukzendaya@gmail.com>
2025-07-12 12:06:45 +0300

    Submit task 5 requirements

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,11 @@
-const arg = process.argv[2];
-const num = parseInt("ALX");
-if (!isNaN(num)) {
-  console.log(`My number: ${num}`);
-} else {
-  console.log("Not a number");
+const inputs = [" ", 89, "89", -4545, "-4545", "ALX"];
+
+for (let i = 0; i < inputs.length; i++) {
+  const num = parseInt(inputs[i]);
+
+  if (!isNaN(num)) {
+    console.log(`My number: ${num}`);
+  } else {
+    console.log("Not a number");
+  }
 }
