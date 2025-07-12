7a1efd6387ea2268d691a88aad922f0e17bc35e9
not-zendaya <ukzendaya@gmail.com>
2025-07-12 12:16:51 +0300

    Fix: submit arguments with correct output

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,8 @@
-const inputs = [" ", 89, "89", -4545, "-4545", "ALX"];
+const inputs = [89, "89", -4545, "-4545", "ALX"];
 
 for (let i = 0; i < inputs.length; i++) {
-  const num = parseInt(inputs[i]);
-
+  const value = inputs[i];
+  const num = parseInt(value);
   if (!isNaN(num)) {
     console.log(`My number: ${num}`);
   } else {
