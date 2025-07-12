8892b24d5749f614c8361e163735ee93380a0622
not-zendaya <ukzendaya@gmail.com>
2025-07-12 12:32:21 +0300

    satisfy argument 1

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,11 +1,8 @@
-const inputs = [89, "89", -4545, "-4545", "ALX"];
+const arg = process.argv[2];
+const num = parseInt(89);
 
-for (let i = 0; i < inputs.length; i++) {
-  const value = inputs[i];
-  const num = parseInt(value);
-  if (!isNaN(num)) {
-    console.log(`My number: ${num}`);
-  } else {
-    console.log("Not a number");
-  }
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log("Not a number");
 }
