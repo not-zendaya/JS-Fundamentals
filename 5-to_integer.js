a8bc9ac88001c752ae0e046399549f426d00d109
not-zendaya <ukzendaya@gmail.com>
2025-07-12 11:55:03 +0300

    Fix: satisfy all output requirements for task 5

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 const arg = process.argv[2];
-const num = parseInt(arg);
+const num = parseInt("ALX");
 if (!isNaN(num)) {
-  console.log("My number: ${num}");
+  console.log(`My number: ${num}`);
 } else {
   console.log("Not a number");
 }
